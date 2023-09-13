558fd76487657ae4fae246c946a749711221d4e6
watish <watish666@gmail.com>
2023-09-13 12:43:12 +0800

    更新了Nginx版本 修正了session目录权限问题

--- php-fpm-fastadmin/entrypoint.sh
+++ php-fpm-fastadmin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-php-fpm7.4 -c /etc/php/7.4/fpm/php-fpm.conf
+php-fpm7.2 -c /etc/php/7.2/fpm/php-fpm.conf
 nginx -c /etc/nginx/nginx.conf
 chmod 777 /data/www/html -R
 echo "Started..."
-tail -f /var/log/access.log
+tail -f /var/log/nginx/access.log
